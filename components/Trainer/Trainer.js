b9149065651feb9e33f7283d1d71dfcc3677cd17
Nora Kauczor <n.kauczor@web.de>
2024-08-01 21:32:35 +0200

    start writing trainer

--- components/Trainer/Trainer.js
+++ components/Trainer/Trainer.js
@@ -1,1 +1,57 @@
-export default function Trainer() {}
+import { useState } from "react";
+import * as styles from "./Trainer.styles";
+
+export default function Trainer(set, moveVocableUp) {
+  if (!set) return <div>Loading...</div>;
+  const [question, setQuestion] = useState(true);
+  const [message, setMessage] = useState("");
+  const [index, setIndex] = useState(0);
+
+  function isAnswerCorrect(input) {
+    const answer = input;
+    if (answer === set.vocables[index].vocable) {
+      return true;
+    }
+    const answerWithoutSpace = answer.split(" ").join("");
+    if (answerWithoutSpace === set.vocables[index].vocable) {
+      return true;
+    }
+    return false;
+  }
+
+  function handleSubmit(event) {
+    setQuestion(false);
+    const correctAnswer = isAnswerCorrect(event.target.answer);
+    if (correctAnswer) {
+      setMessage("Correct!");
+    } else {
+      setMessage("False :(");
+      moveVocableUp(currentVocable);
+    }
+  }
+
+  function goToNextVocable() {
+    setQuestion(true);
+    const oldIndex = [...index];
+    setIndex(oldIndex + 1);
+  }
+
+  return (
+    <styles.TrainerWrapper>
+      {message.length !== 0 && <styles.TrainerMessage></styles.TrainerMessage>}
+      {question ? (
+        <styles.VocableCard>
+          {set.vocables[index].translation}
+        </styles.VocableCard>
+      ) : (
+        <styles.VocableCard>{set.vocables[index].vocable}</styles.VocableCard>
+      )}
+      <styles.TrainerForm>
+        <AnswerField name="answer" />
+        <styles.CheckButton onSubmit={handleSubmit}>
+          Check my answer
+        </styles.CheckButton>
+      </styles.TrainerForm>
+    </styles.TrainerWrapper>
+  );
+}
---
+++ components/Trainer/Trainer.styles.js
@@ -0,0 +1,7 @@
+const { default: styled } = require("styled-components");
+export const TrainerWrapper = styled.div``;
+export const TrainerMessage = styled.p``;
+export const VocableCard = styled.p``;
+export const TrainerForm = styled.form``;
+export const AnswerField = styled.input``;
+export const CheckButton = styled.button``;
